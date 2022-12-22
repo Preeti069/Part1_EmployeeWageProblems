f59cffdf5d77dd914fd261f5074c81581f41a1e7
preeti <pritich0896@gmail.com>
2022-12-22 18:22:31 +0530

    using switch case

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -8,36 +8,44 @@ namespace Employeewageproblem
 {
     public class EmployeeCheck
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
 
-
-        public static void CheckAttendance()
-
+        public static void CheckAttedence()
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
             int empHrs = 0;
-            int EMP_RATE_PER_HOUR = 20;
             int empWage = 0;
+            int totalEmpWage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(3);
-
-            if (empCheck == IS_FULL_TIME)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
+                Random random = new Random();
+                int empCheck = random.Next(3);
 
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 0;
-            }
-            else
-            {
-                empHrs = 0;
-            }
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;                                                                                                                                        
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee wage : " + empWage);
+                }
+
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empCheck;
+
+                Console.WriteLine("EmpWage : " + empWage);
+
+            }
+            Console.WriteLine("Total emp wage :" + totalEmpWage);
         }
+               
     }
 }
