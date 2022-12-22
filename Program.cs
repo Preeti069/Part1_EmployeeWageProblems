39c13f2646c2dc3ebebf9bfbf180b2957d27d6df
preeti <pritich0896@gmail.com>
2022-12-22 14:56:30 +0530

    UC1_CheckAttedence

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿namespace employeeWageProblem
+﻿using EmployeeWageProblem;
+
+namespace employeeWageProblem
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
-
+            EmployeeCheck.CheckAttendence();
         }
     }
 }
